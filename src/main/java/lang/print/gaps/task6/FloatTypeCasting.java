afe7e96147efc059921c84dc49caead471e7b223
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-06-27 15:10:52 +0500

    fixed

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        int a = (int) Math.ceil(numberToBeRounded);
+        int a = Math.round(numberToBeRounded);
         System.out.println(a);
     }
 }
